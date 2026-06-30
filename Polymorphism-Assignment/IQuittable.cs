7f2d0a78e15371388fe4a0795fff17d901973147
ZehraTosun1 <ztosun1903@gmail.com>
2026-06-30 00:22:04 -0400

    IQuittable.cs

---
+++ Polymorphism-Assignment/IQuittable.cs
@@ -0,0 +1,9 @@
+namespace PolymorphismAssignment
+{
+    // This interface defines behavior for any class that can quit.
+    public interface IQuittable
+    {
+        // This method must be implemented by any class that uses this interface.
+        void Quit();
+    }
+}
